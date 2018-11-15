0bd324ddbae0734a8e17174170c20c55bc96f15c
ullf <sokomark1412@gmail.com>
2018-11-15 12:49:53 +0100

    test fit

---
+++ src/main/java/ru/marksblog/view/Account.java
@@ -0,0 +1,33 @@
+package ru.marksblog.view;
+
+import com.vaadin.flow.component.ClickEvent;
+import com.vaadin.flow.component.ComponentEventListener;
+import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.html.Label;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.component.textfield.TextField;
+import com.vaadin.flow.router.Route;
+import org.springframework.beans.factory.annotation.Autowired;
+import ru.marksblog.service.UserService;
+
+@Route("account")
+public class Account extends VerticalLayout{
+
+    @Autowired
+    private UserService userService;
+
+    public Account(){
+        Label label = new Label("Delete user");
+        TextField fieldUsername = new TextField("Username:");
+        Button button = new Button("delete");
+        button.addClickListener(new ComponentEventListener<ClickEvent<Button>>() {
+            @Override
+            public void onComponentEvent(ClickEvent<Button> buttonClickEvent) {
+                userService.deleteByUsername(fieldUsername.getValue());
+            }
+        });
+        add(label);
+        add(fieldUsername);
+        add(button);
+    }
+}
